89096f7fc524b63a15ac46594d71a012bc560406
Anand Dyavanapalli <adyavanapalli@gmail.com>
2016-07-13 17:04:01 -0400

    Removed sudo and added make -j
    sudo is unnecessary when running make in a local directory; added make -j option to support multi-threaded machines to speedup script

--- WNIC.sh
+++ WNIC.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 cd backports-20150731/ && \
-sudo make defconfig-ath10k && \
-sudo make install && \
+make -j defconfig-ath10k && \
+sudo make -j install && \
 cd ../ath10k-firmware-master/ && \
 sudo cp -R ath10k/ /lib/firmware && \
 sudo bash -c 'echo "options ath10k_core skip_otp=y" > /etc/modprobe.d/ath10k.conf' && \
